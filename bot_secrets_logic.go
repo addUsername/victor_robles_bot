ade8526fab069e1b04a79a3ab3f68d4544a9e821
narvikd <84069271+narvikd@users.noreply.github.com>
2021-06-24 01:11:04 +0200

    Implemented token retrieval logic using Vault

---
+++ bot_secrets_logic.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hashicorp/vault/api"
+	"os"
+)
+
+func getToken() string {
+	vaultAddr := os.Getenv("vaultAddr")
+	vaultToken := os.Getenv("vaultToken")
+	secretLocation := "api"
+	config := &api.Config{
+		Address: vaultAddr,
+	}
+
+	// Starts the client and logs in
+	client, errClient := api.NewClient(config)
+	handleError(errClient, true)
+	client.SetToken(vaultToken)
+
+	// Reads the secret data
+	secret, errSecret := client.Logical().Read("secret/data/" + secretLocation)
+	handleError(errSecret, true)
+
+	// Maps the secret data so we can access the parts we need (the decrypted key value)
+	mapping, errMap := secret.Data["data"].(map[string]interface{})
+	if !errMap {
+		log.Fatalln(errMap)
+	}
+	// We convert the key value to string and finally we return it
+	token := fmt.Sprintf("%v", mapping[secretLocation])
+
+	return token
+}
