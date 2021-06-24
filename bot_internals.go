9119a00f094639418be089ae8fad7ad07f97ad18
narvikd <84069271+narvikd@users.noreply.github.com>
2021-06-24 15:00:52 +0200

    Remove hardcoded senderID and added comments

--- bot_internals.go
+++ bot_internals.go
@@ -9,7 +9,7 @@ func createBot() *tb.Bot {
 	// Creates a bot using the default URL setting (https://api.telegram.org), this can be overwritten if desired.
 	bot, err := tb.NewBot(tb.Settings{
 		Token: getToken(),
-		// The 10 second timeout it's the default and recommended setting, a lower number seems to affect performance.
+		// The 10 second timeout is the default and recommended setting, a lower number seems to affect performance.
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
 	handleError(err, "fatal")
@@ -25,9 +25,14 @@ func createBot() *tb.Bot {
 
 func handleEndpoint(bot *tb.Bot, route string, message string, privateMsg bool) {
 	bot.Handle(route, func(src *tb.Message) {
+		// chatID represents the chat where the message was sent, which is not the same as the user than sent the message.
 		chatID := tb.ChatID(src.Chat.ID)
+		senderID := tb.ChatID(src.Sender.ID)
+
 		if privateMsg {
-			_, errSend := bot.Send(src.Sender, message, "html")
+			// It cannot be substituted for sendMessage() because it needs to retrieve the error in order to
+			// check if the bot doesn't have permissions to send a message to the user (error 403).
+			_, errSend := bot.Send(senderID, message, "html")
 			if !handleError403(bot, chatID, errSend) {
 				handleError(errSend, "error")
 				sendMessage(bot, chatID, "Te lo he enviado por privado shur")
@@ -47,7 +52,7 @@ func sendMessage(bot *tb.Bot, chatID tb.ChatID, message string) {
 }
 
 func sendMessageToAdmin(bot *tb.Bot, message string) {
-	// This is hardcoded for now, since I'm the only admin
+	// This is hardcoded for now, since I'm the only admin.
 	chatID := tb.ChatID(1099020633)
 	sendMessage(bot, chatID, message)
 	log.Warnln("Message: " + message + ". Sent to admins.")
