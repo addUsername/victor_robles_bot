4058837f63d2cc58f85a882d4c9f816d53aea378
narvikd <84069271+narvikd@users.noreply.github.com>
2021-06-24 15:10:25 +0200

    Remove hardcoded sendMessage, added comments and more error types

--- error_handling.go
+++ error_handling.go
@@ -9,19 +9,25 @@ func handleError(err error, errType string) {
 	if err != nil {
 		if errType == "fatal" {
 			log.Fatalln(err)
+		} else if errType == "panic" {
+			log.Panicln(err)
 		} else if errType == "error" {
 			log.Errorln(err)
 		} else if errType == "warn" {
 			log.Warnln(err)
 		} else if errType == "debug" {
 			log.Debugln(err)
+		} else if errType == "info" {
+			log.Infoln(err)
 		}
 	}
 }
 
 func handleError403(bot *tb.Bot, chatID tb.ChatID, err error) bool {
+	const textTooLongErrMsg = "El texto que te quiero enviar es un poco largo, " +
+		"ábreme un privado a @victor_robles_bot para poder mandártelo."
 	if err != nil && is403(err) {
-		sendMessage(bot, chatID, "El texto que te quiero enviar es un poco largo, no te lo puedo enviar si no me abres un privado @victor_robles_bot")
+		sendMessage(bot, chatID, textTooLongErrMsg)
 		handleError(err, "error")
 		return true
 	}
@@ -32,6 +38,7 @@ func is403(err error) bool {
 	regexExpr := "403"
 	regex, errCompile := regexp.Compile(regexExpr)
 	handleError(errCompile, "error")
+	// err.Error() is an interface for the String contents of the Error type.
 	match := regex.MatchString(err.Error())
 	return match
 }
